90b01043ce3d1cf1953972c5ec07db118361baa8
Jan Vansteenkiste <jan@vstone.eu>
2012-01-30 10:59:59 +0100

    Fixed initial sql to create the database structure.

--- urlwiz.sql
+++ urlwiz.sql
@@ -1,13 +1,15 @@
-
 --
 -- Table structure for table `tag`
 --
 
-CREATE TABLE IF NOT EXISTS `tag` (
-  `tagid` int(10) unsigned NOT NULL COMMENT 'TAG ID',
+CREATE TABLE `tag` (
+  `tagid` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'TAG ID',
   `tag` varchar(100) NOT NULL COMMENT 'Tag',
-  PRIMARY KEY  (`tagid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `count` int(10) unsigned NOT NULL DEFAULT '1',
+  PRIMARY KEY (`tagid`),
+  UNIQUE KEY `tag` (`tag`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+
 
 -- --------------------------------------------------------
 
@@ -15,16 +17,18 @@ CREATE TABLE IF NOT EXISTS `tag` (
 -- Table structure for table `url`
 --
 
-CREATE TABLE IF NOT EXISTS `url` (
-  `url_id` int(10) unsigned NOT NULL COMMENT 'URL ID',
+CREATE TABLE `url` (
+  `url_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'URL ID',
   `url` varchar(255) NOT NULL COMMENT 'Link (duh)',
+  `title` varchar(255) NOT NULL,
   `tinyurl` varchar(50) NOT NULL COMMENT 'TinyURL for this link (if there was one generated)',
   `nick` varchar(32) NOT NULL COMMENT 'Who mentioned it?',
-  `reffed` varchar(32) default NULL COMMENT 'Who was reffed?',
+  `reffed` varchar(32) DEFAULT NULL,
   `channel` varchar(32) NOT NULL COMMENT 'Where was it mentioned?',
   `date` int(10) unsigned NOT NULL COMMENT 'When was it mentioned',
-  PRIMARY KEY  (`url_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`url_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+
 
 -- --------------------------------------------------------
 
@@ -32,12 +36,11 @@ CREATE TABLE IF NOT EXISTS `url` (
 -- Table structure for table `urltag`
 --
 
-CREATE TABLE IF NOT EXISTS `urltag` (
+CREATE TABLE `urltag` (
   `url_id` int(10) unsigned NOT NULL,
   `tag_id` int(10) unsigned NOT NULL,
-  PRIMARY KEY  (`url_id`,`tag_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-ALTER TABLE `urltag`
-  ADD CONSTRAINT `urltag_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`tagid`) ON DELETE CASCADE,
-  ADD CONSTRAINT `urltag_ibfk_1` FOREIGN KEY (`url_id`) REFERENCES `url` (`url_id`) ON DELETE CASCADE;
+  PRIMARY KEY (`url_id`,`tag_id`),
+  KEY `tag_id` (`tag_id`),
+  CONSTRAINT `urltag_ibfk_1` FOREIGN KEY (`url_id`) REFERENCES `url` (`url_id`) ON DELETE CASCADE,
+  CONSTRAINT `urltag_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`tagid`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
